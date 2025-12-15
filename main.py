a75b432b18a0e0e6218ef46949bfe21c08631f68
alpikii <alpikii@github.com>
2025-12-15 13:50:12 +0200

    final

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 import network
 import socket
 
-# --- PIR + LED ---
+# PIR + LED setup
 pir = Pin(0, Pin.IN, Pin.PULL_DOWN)
 led = Pin(2, Pin.OUT)
 
@@ -11,32 +11,39 @@ counter = 0
 pir_state = False
 last_motion = 0
 debounce = 0.5
+valkkumisvali = 0
 
-# --- WiFi ---
-SSID = "wifi314too"
-PASSWORD = "220473-314"
+def blink(times=5, delay=0.1):
+    for _ in range(times):
+        led.on(); time.sleep(delay)
+        led.off(); time.sleep(delay)
+
+# WiFi setup
+SSID = "OTiT"
+PASSWORD = "oh8taoh8ta"
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(SSID, PASSWORD)
 
 while wlan.status() != 3:
-    print("Connecting...")
+    #print("Connecting...")
     time.sleep(1)
 
 ip = wlan.ifconfig()[0]
-print("Connected:", ip)
+#print("Connected:", ip)
+blink(3, 0.1)
 
-# --- HTML page (optional) ---
+# HTML
 def generate_html():
     return """<html><body><h1>Pico Counter</h1></body></html>"""
 
-# --- Server ---
+# Server setup
 s = socket.socket()
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(('0.0.0.0', 80))
 s.listen(1)
-print("Listening on port 80")
+#print("Listening on port 80")
 
 while True:
     # PIR detection
@@ -47,8 +54,9 @@ while True:
         pir_state = True
         counter += 1
         last_motion = now
-        led.on()
-        print("Motion:", counter)
+        if valkkumisvali > 0 and (counter % valkkumisvali == 0):
+            blink()
+        #print("Motion:", counter)
 
     if val == 0 and pir_state and now - last_motion > debounce:
         pir_state = False
@@ -61,12 +69,31 @@ while True:
         #print("Client connected:", addr)
 
         request = cl.recv(512).decode()
+        
 
         if "GET /count" in request:
             cl.send("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n")
             cl.send(str(counter))
             cl.close()
             continue
+        
+        # Handle GET /setblink?value=N
+        if "GET /setblink" in request:
+            try:
+                # Extract number after ?value=
+                part = request.split("value=")[1]
+                newval = int(part.split()[0])
+                valkkumisvali = newval
+                #print("Blink interval updated to:", valkkumisvali)
+
+                cl.send("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n")
+                cl.send("OK")
+                cl.close()
+                continue
+            except:
+                cl.send("HTTP/1.1 400 Bad Request\r\n\r\n")
+                cl.close()
+                continue
 
         # Serve normal page
         cl.send("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n")
